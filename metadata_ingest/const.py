2b695ba9112581169aef7e642af58718c0e3c974
Julia Lien <lien_julia@bah.com>
2019-05-10 16:49:33 -0400

    Temp solution to allow for import of local const config

--- metadata_ingest/const.py
+++ metadata_ingest/const.py
@@ -1,6 +1,21 @@
-# Add your credentials
+"""
+Modify the config section below, OR copy the CONFIG SECTION to a new file in
+this folder called "const_local.py".
+"""
+
+import os
+
+### CONFIG SECTION STARTS ###
 
 SOCRATA_DOMAIN = 'data.transportation.gov'
 SOCRATA_USERNAME = None
 SOCRATA_PASSWORD = None
 SOCRATA_API_KEY = None
+
+### CONFIG SECTION ENDS ###
+
+# check for local const file
+dir_path = os.path.dirname(os.path.realpath(__file__))
+local_const_path = os.path.join(dir_path, 'const_local.py')
+if SOCRATA_USERNAME is None and os.path.exists(local_const_path):
+    from const_local import *
